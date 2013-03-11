11e66524038e52d2848eb36b7385e75d85910d88
Javier Ferreiro <javier.ferreiro@gmail.com>
2013-03-11 12:54:32 +0100

    Creacion de nuevas actividades (I) Primera version con novedades:  *Boton "Acerca De" Funcional y con estilo  *Boton "Salir" declarado mediante Java (Version primera).
    modified:   AndroidManifest.xml
    modified:   gen/org/example/asteroides/R.java
    new file:   res/layout/acercade.xml
    modified:   res/layout/main.xml
    new file:   src/org/example/asteroides/AcercaDe.java
    modified:   src/org/example/asteroides/Asteroides.java

--- src/org/example/asteroides/Asteroides.java
+++ src/org/example/asteroides/Asteroides.java
@@ -1,15 +1,38 @@
 package org.example.asteroides;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Asteroides extends Activity {
-
+	private Button bAcercaDe;
+	private Button bSalir;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //Listeners "Acerca de"
+        bAcercaDe=(Button) findViewById(R.id.button3);
+        bAcercaDe.setOnClickListener(new OnClickListener(){
+        	public void onClick(View view){
+        		lanzarAcercaDe(null);
+        	}
+  
+        });
+        
+        //Listeners "Salir"
+        bSalir=(Button) findViewById(R.id.button4);
+        bSalir.setOnClickListener(new OnClickListener(){
+        	public void onClick(View view){
+        		lanzarSalir();
+        	}
+        });
     }
 
     @Override
@@ -17,6 +40,15 @@ public class Asteroides extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void lanzarAcercaDe(View view){
+    	Intent i = new Intent(this, AcercaDe.class);
+    	startActivity(i);
+    }
+    
+    public void lanzarSalir(){
+    	finish();
     }
     
 }
