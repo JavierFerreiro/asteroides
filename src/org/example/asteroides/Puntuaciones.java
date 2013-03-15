d1e540c42f1a634d9d01c4e85010b8046fbf1a58
Javier Ferreiro <javier.ferreiro@gmail.com>
2013-03-15 14:25:43 +0100

    Detectar una pulsacion sobre un elemento de la lista
    On branch master
    modified:   src/org/example/asteroides/Puntuaciones.java

--- src/org/example/asteroides/Puntuaciones.java
+++ src/org/example/asteroides/Puntuaciones.java
@@ -2,7 +2,9 @@ package org.example.asteroides;
 
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Puntuaciones extends ListActivity{
 	@Override public void onCreate(Bundle saveInstanceState){
@@ -18,4 +20,12 @@ public class Puntuaciones extends ListActivity{
 												);
        */
 	}
+	
+	@Override protected void onListItemClick(ListView listView, 
+            View view, int position, long id) {
+		super.onListItemClick(listView, view, position, id);
+		Object o = getListAdapter().getItem(position);
+		Toast.makeText(this, "Selección: " + Integer.toString(position)
+				+  " - " + o.toString(),Toast.LENGTH_LONG).show();
+}
 }
