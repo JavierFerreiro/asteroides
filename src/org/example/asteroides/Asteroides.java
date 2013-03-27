1fc06d80964094dc2d98ff8fd94b355742dfbc5d
Javier Ferreiro Espinosa <javier.ferreiro@gmail.com>
2013-03-27 11:00:43 +0100

    La clase VistaJuego
    On branch master
    modified:   AndroidManifest.xml
    modified:   res/layout/main.xml
    modified:   src/org/example/asteroides/Asteroides.java
    modified:   src/org/example/asteroides/Grafico.java
    new file:   src/org/example/asteroides/VistaJuego.java

--- src/org/example/asteroides/Asteroides.java
+++ src/org/example/asteroides/Asteroides.java
@@ -67,6 +67,11 @@ public class Asteroides extends Activity {
     	startActivity(i);
     }
     
+    public void lanzarJuego(View view){
+    	Intent i=new Intent(this,Juego.class);
+    	startActivity(i);
+    }
+    
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
     	switch(item.getItemId() ){
