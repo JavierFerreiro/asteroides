82c0abb98f871b6777798885fada2beb7b53a742
Javier Ferreiro <javier.ferreiro@gmail.com>
2013-04-04 12:26:09 +0200

    Manejo de la nave con pantalla tactil
    On branch master
    modified:   .gitignore
    modified:   src/org/example/asteroides/VistaJuego.java

--- src/org/example/asteroides/VistaJuego.java
+++ src/org/example/asteroides/VistaJuego.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class VistaJuego extends View{
@@ -29,6 +30,9 @@ public class VistaJuego extends View{
     private static int PERIODO_PROCESO = 50;
     // Cuando se realizó el último proceso
     private long ultimoProceso = 0;
+    
+    private float mX=0, mY=0;
+    private boolean disparo=false;
     
     public VistaJuego(Context context, AttributeSet attrs) {
     	super(context, attrs);
@@ -110,7 +114,7 @@ public class VistaJuego extends View{
         for (Grafico asteroide : Asteroides) {
               asteroide.incrementaPos(retardo);
         }
- }
+       }
     
     class ThreadJuego extends Thread {
     	   @Override
@@ -120,4 +124,42 @@ public class VistaJuego extends View{
     	          }
     	   }
     	}
+
+    @Override public boolean onTouchEvent (MotionEvent event){
+    	super.onTouchEvent(event);
+    	
+    	float x=event.getX();
+    	float y=event.getY();
+    	
+    	switch(event.getAction()){
+    	case MotionEvent.ACTION_DOWN:
+    		disparo=true;
+    		break;
+    	case MotionEvent.ACTION_MOVE:
+    		float dx=Math.abs(x-mX);
+    		float dy=Math.abs(y-mY);
+    		
+    		if(dy<6 && dx>6){ //Motion in horizontal on the screen, then spin the ship
+    			giroNave = Math.round((x-mX)/2);
+    			disparo=false;
+    		}else if(dx<6 && dy>6){
+    			aceleracionNave=Math.round((mY-y)/25);
+    			disparo=false;
+    		}
+    		break;
+    	case MotionEvent.ACTION_UP:
+    		giroNave=0;
+    		aceleracionNave=0;
+    		
+    		if(disparo){
+    		// In the future:	ActivaMisil();
+    		}
+    		
+    		break;
+    		
+    	}
+    	
+    	mX=x;mY=y;
+    	return true;
+    }
 }
